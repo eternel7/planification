cf4be1036a8069808f996ef425b83c9ab8521799
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-02-11 21:50:15 +0000

    format

--- trunk/ planification --username eternel7/planification/application/models/site_model.php
+++ trunk/ planification --username eternel7/planification/application/models/site_model.php
@@ -1,19 +1,15 @@
 <?php
 
-class site_model extends Model 
-{
-	function Getall()
-	{
-		$q = $this->db->get('test');
-		
-		if ($q->num_rows()>0)
-		{
-			foreach ($q->result() as $row)
-			{
-				$data[]=$row;
-			}
-		}
-		return $data;
-	}
-	
+class site_model extends Model {
+    function Getall() {
+        $q = $this->db->get('test');
+
+        if ($q->num_rows()>0) {
+            foreach ($q->result() as $row) {
+                $data[]=$row;
+            }
+        }
+        return $data;
+    }
+
 }
