cf80665939f1b2d1fb8d503d3ea6dda257501149
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-03-03 22:59:25 +0000

    login presque finish

---
+++ trunk/ planification --username eternel7/planification/application/language/french/site_lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['site_restricted_area_warning'] = "Section prot&eacute;g&eacute;e du site";
+$lang['site_welcome_title1'] = "Bienvenue ";
+$lang['site_welcome_title2'] = "!";
+$lang['site_welcome_message'] = "This section represents the area that only logged in members can access.";
+$lang['site_logout'] = "D&eacute;connection";
