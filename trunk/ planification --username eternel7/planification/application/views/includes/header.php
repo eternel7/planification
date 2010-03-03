fee51178e52dae7e0e3274217a5e2f45b00e84b9
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-03-03 22:58:37 +0000

    login presque finish

---
+++ trunk/ planification --username eternel7/planification/application/views/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>" lang="<?php echo $lang; ?>">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>QCM</title>
+                <meta name="author" content="David MASSE" />
+                <meta name="keywords" content="<?php echo "_META_KEYWORDS"; ?>" />
+                <meta name="description" content="<?php echo "_META_DESCRIPTION"; ?>" />
+                <meta name="robots" content="all" />
+                <meta http-equiv="Content-Script-Type" content="text/javascript" />
+                <meta http-equiv="Content-Style-Type" content="text/css" />
+                <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="all" title="stylesheet" charset="UTF-8" />
+                <link rel="shortcut icon" href="<?php echo base_url();?>images/favicon.ico" type="image/x-icon" />
+                <link rel="icon" href="<?php echo base_url();?>images/favicon-anim.gif" type="image/gif" />
+                <!-- to correct the unsightly Flash of Unstyled Content. http://www.bluerobot.com/web/css/fouc.asp -->
+                <script type="text/javascript"></script>
+	</head>
+	<body>
