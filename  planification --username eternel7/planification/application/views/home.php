c7309e8c3f0e50174c51f4f8ed7a0f5101b4bd5d
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-03-03 22:58:45 +0000

    login presque finish

---  planification --username eternel7/planification/application/views/home.php
+++  planification --username eternel7/planification/application/views/home.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<title>Insert title here</title>
-	</head>
-	<body>
-	
-		<div id="container">
-			<p>My view has been loaded</p>
-			<p><?php echo $Myvalue;?></p>
-			<p><?php echo $Myvalue2;?></p>
-			
-			<p><?php  foreach ($records as $row):?>
-				<h1><?php echo $row->title; ?></h1>
-			<?php endforeach;?></p>
-		</div>
-	
-	</body>
-</html>
+<div id="container">
+    <h1><span><?php echo ($this->lang->line('site_welcome_title1') . $this->session->userdata('username'));?></span></h1>
+    <p><?php echo ($this->lang->line('site_welcome_message'));?></p>
+    <h4><?php echo anchor('login/logout', $this->lang->line('site_logout')); ?></h4>
+</div>
