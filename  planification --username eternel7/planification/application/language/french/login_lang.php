683bc337a92ef5e6959a6eb50aedf26ffa510216
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-03-03 22:59:25 +0000

    login presque finish

---
+++  planification --username eternel7/planification/application/language/french/login_lang.php
@@ -0,0 +1,28 @@
+<?php
+$lang['login_title'] = "Authentification";
+$lang['login_username'] = "Compte :";
+$lang['login_pwd'] = "Mot de passe :";
+$lang['login_submit'] = "Se connecter";
+$lang['login_create_account'] = "Cr&eacute;er un compte";
+$lang['login_create_user_username'] = "Compte";
+$lang['login_create_user_pwd'] = "Mot de passe";
+$lang['login_confirm_pwd'] = "Confirmation du mot de passe";
+$lang['login_email'] = "Email";
+$lang['login_first_name'] = "Nom";
+$lang['login_last_name'] = "Pr&eacute;nom";
+$lang['login_pseudonyme'] = "Pseudonyme";
+$lang['login_description'] = "Description";
+$lang['login_create_user_pwd_alert'] = "Veuillez saisir un mot de passe!";
+$lang['login_confirm_pwd_alert'] = "Mauvaise confirmation du mot de passe!";
+$lang['login_login_info'] = "Information du compte";
+$lang['login_create_user_username_title'] = "Compte";
+$lang['login_create_user_pwd_title'] = "Mot de passe";
+$lang['login_confirm_pwd_title'] = "Confirmation du mot de passe";
+$lang['login_create_user_email_title'] = "email";
+$lang['login_create_user_submit'] = "Créer le compte";
+$lang['login_personnal_info'] = "Information personnelle";
+$lang['login_create_user_first_name_title'] = "Nom";
+$lang['login_create_user_last_name_title'] = "Pr&eacute;nom";
+$lang['login_create_user_pseudonyme_title'] = "Pseudonyme";
+$lang['login_create_user_description_title'] = "Description";
+
