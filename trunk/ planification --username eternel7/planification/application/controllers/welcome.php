668f13b133f6ebc666170025af477542e95162e9
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-04-21 20:21:19 +0000

    color

--- trunk/ planification --username eternel7/planification/application/controllers/welcome.php
+++ trunk/ planification --username eternel7/planification/application/controllers/welcome.php
@@ -20,6 +20,14 @@ class Welcome extends Controller {
             $data['main_content']='test_1';
             $this->load->view('includes/template_960cssgrid',$data);
         }
+        function test2()
+        {
+            $data = array();
+            //Chargement du parametre de langue
+            $data['lang']=$this->config->item('lang');
+            $data['main_content']='test_2';
+            $this->load->view('test_2');
+        }
 }
 
 /* End of file welcome.php */
