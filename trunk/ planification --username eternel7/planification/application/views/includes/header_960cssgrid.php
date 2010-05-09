3d0f90a8994e032bb719c89dce3bd8fbbfee90a1
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-05-09 18:57:12 +0000

    extjs

--- trunk/ planification --username eternel7/planification/application/views/includes/header_960cssgrid.php
+++ trunk/ planification --username eternel7/planification/application/views/includes/header_960cssgrid.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>" lang="<?php echo $lang; ?>">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" Content="text/html; charset=utf-8">
         <title id='title'>HTML Page setup Tutorial</title>
         <meta name="author" content="David MASSE" />
         <meta name="keywords" content="<?php echo "_META_KEYWORDS"; ?>" />
         <meta name="description" content="<?php echo "_META_DESCRIPTION"; ?>" />
         <meta name="robots" content="all" />
-        <meta http-equiv="Content-Script-Type" content="text/javascript" />
-        <meta http-equiv="Content-Style-Type" content="text/css" />
         <!-- ** CSS ** -->
         <!-- base library -->
         <link rel="stylesheet" href="<?php echo base_url();?>javascript/jQuery/css/jquery-ui.css" type="text/css" media="all" />
         <link rel="stylesheet" href="<?php echo base_url();?>javascript/jQuery/css/ui-lightness/jquery-ui-1.8rc3.custom.css" type="text/css" media="all" />
         
         <!-- overrides to base library -->
-        <!-- <link rel="stylesheet" href="<?php echo base_url();?>css/960cssgrid.css" type="text/css" media="all" title="stylesheet" charset="UTF-8" /> -->
         <link rel="stylesheet" href="<?php echo base_url();?>css/jnote_le.css" type="text/css" media="all" title="stylesheet" charset="UTF-8" />
-        <!-- <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>javascript/jQuery/richtexteditor/jquery.rte.css" /> -->
 
         <!-- extentions -->
         <link rel="stylesheet" href="<?php echo base_url();?>javascript/jQuery/extensions/farbtastic12/farbtastic.css" type="text/css" />
@@ -27,4 +23,5 @@
         <link rel="icon" href="<?php echo base_url();?>images/favicon-anim.gif" type="image/gif" />
     </head>
     <body>
+
         
