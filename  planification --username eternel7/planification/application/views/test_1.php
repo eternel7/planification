75cda767f519d2c5f1ee90f630d11230b6f33957
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-05-09 18:57:39 +0000

    extjs

---  planification --username eternel7/planification/application/views/test_1.php
+++  planification --username eternel7/planification/application/views/test_1.php
@@ -1,6 +1,6 @@
 
 <div id="notes_action_bar">
-    <span id="opener" class="notes-opener notes-effect"><img src="<?php echo base_url(); ?>images/post-it.jpg" height="75px" width="75px" alt="Create a note" /><input type="hidden" id="z_index_max" name="z_index_max" value="0" /></span>
+    <span id="opener" class="notes-opener notes-effect"><img src="<?php echo base_url(); ?>images/post-it.jpg" height="75" width="75" alt="Create a note" /><input type="hidden" id="z_index_max" name="z_index_max" value="0" /></span>
     <span id="reducer" class="notes-reducer notes-effect">Reduce Notes</span>
     <span id="saver" class="notes-saver notes-effect">Save Notes</span>
     <span id="trasher" class="notes-trasher notes-effect"><img src="<?php echo base_url(); ?>images/trasher.jpg" alt="Bin" /></span>
@@ -9,6 +9,23 @@
     <span class="notes-effect">
         <span class="notes-effect-element">Toggle method :</span>
         <span class="notes-effect-element"><select name="effectTypes" id="effectTypes">
+            <option value="blind">Blind</option>
+            <option value="bounce">Bounce</option>
+            <option value="clip">Clip</option>
+            <option value="drop">Drop</option>
+            <option value="explode">Explode</option>
+            <option value="fold">Fold</option>
+            <option value="highlight">Highlight</option>
+            <option value="puff">Puff</option>
+            <option value="pulsate">Pulsate</option>
+            <option value="scale">Scale</option>
+            <option value="size">Size</option>
+            <option value="slide">Slide</option>
+        </select></span>
+    </span>
+    <span class="notes-effect">
+        <span class="notes-effect-element">Close method :</span>
+        <span class="notes-effect-element"><select name="effectTypesclose" id="effectTypesclose">
             <option value="blind">Blind</option>
             <option value="bounce">Bounce</option>
             <option value="clip">Clip</option>
