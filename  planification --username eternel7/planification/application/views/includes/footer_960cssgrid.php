ec52e892e06bf30a55b482384b6cd6e779e87482
eternel7 <eternel7@6e0d097a-136c-11df-8fcd-d121000d4eca>
2010-05-09 18:57:16 +0000

    extjs

---  planification --username eternel7/planification/application/views/includes/footer_960cssgrid.php
+++  planification --username eternel7/planification/application/views/includes/footer_960cssgrid.php
@@ -9,11 +9,9 @@
 
         <!-- extensions -->
         <script type="text/javascript" src="<?php echo base_url();?>javascript/jQuery/extensions/farbtastic12/farbtastic.js"></script>
-        <!-- <script type="text/javascript" src="<?php echo base_url();?>javascript/jQuery/extensions/tinysort.1.0.4/jquery.tinysort.min.js"></script> -->
+        <script type="text/javascript" src="<?php echo base_url();?>javascript/jQuery/extensions/jquery.cookie.js"></script>
         
         <!-- jQuery or ExtJS homemade page specific -->
         <script type="text/javascript" src="<?php echo base_url();?>javascript/jnote_le.js"></script>
-        <!-- <script type="text/javascript" src="<?php echo base_url();?>javascript/960griddesign.js"></script> -->
-        <!-- <script type="text/javascript" src="http://jqueryui.com/themeroller/themeswitchertool/"></script> -->
     </body>
 </html>
